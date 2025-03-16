6b1b07cbeec3369dc8f4b9df0da3d893bdce370b
shelimingming <shelimingming@gmail.com>
2025-03-16 19:26:09 +0800

    更新时间管理大师应用的设计文档和配置

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -4,21 +4,52 @@
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view>
+		
+		<view class="button-group">
+			<button type="primary" class="login-btn" @click="toLoginPage">登录</button>
+			<button type="default" class="userinfo-btn" @click="toUserinfoPage">个人中心</button>
+		</view>
 	</view>
 </template>
 
 <script>
+	import { store } from '@/uni_modules/uni-id-pages/common/store.js'
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Chronos 时间管理大师',
+				userInfo: {}
+			}
+		},
+		computed: {
+			hasLogin() {
+				return store.hasLogin
 			}
 		},
 		onLoad() {
-
+			this.userInfo = store.userInfo
 		},
 		methods: {
-
+			toLoginPage() {
+				uni.navigateTo({
+					url: '/uni_modules/uni-id-pages/pages/login/login-withoutpwd'
+				})
+			},
+			toUserinfoPage() {
+				if (!this.hasLogin) {
+					uni.showToast({
+						title: '请先登录',
+						icon: 'none'
+					})
+					setTimeout(() => {
+						this.toLoginPage()
+					}, 1500)
+					return
+				}
+				uni.navigateTo({
+					url: '/uni_modules/uni-id-pages/pages/userinfo/userinfo'
+				})
+			}
 		}
 	}
 </script>
@@ -43,10 +74,20 @@
 	.text-area {
 		display: flex;
 		justify-content: center;
+		margin-bottom: 50rpx;
 	}
 
 	.title {
 		font-size: 36rpx;
 		color: #8f8f94;
+	}
+	
+	.button-group {
+		width: 80%;
+		margin-top: 50rpx;
+	}
+	
+	.login-btn, .userinfo-btn {
+		margin-bottom: 30rpx;
 	}
 </style>
--- uni_modules/uni-id-pages/config.js
+++ uni_modules/uni-id-pages/config.js
@@ -47,7 +47,9 @@ export default {
       // 微信公众号的appid，来源:登录微信公众号（https://mp.weixin.qq.com）-> 设置与开发 -> 基本配置 -> 公众号开发信息 -> AppID
       h5: 'xxxxxx',
       // 微信开放平台的appid，来源:登录微信开放平台（https://open.weixin.qq.com） -> 管理中心 -> 网站应用 -> 选择对应的应用名称，点击查看 -> AppID
-      web: 'xxxxxx'
+      web: 'xxxxxx',
+      // 微信小程序appid，与uni-config-center中的配置保持一致
+      mp: 'wx91e250891f2dec60'
     }
   },
   /**
