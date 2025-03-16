523f1caa23e68ac0268265f1bd34bb1acc33b84d
shelimingming <shelimingming@gmail.com>
2025-03-16 19:41:47 +0800

    增加页面

---
+++ pages/statistics/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="container">
+		<text>统计页面</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.container {
+	padding: 20px;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
+</style>
